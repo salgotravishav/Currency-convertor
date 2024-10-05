d7830838f986dfa76239269cb9a068c8cf1a26c4
salgotravishav <salgotravishav2002@gmail.com>
2024-10-05 22:44:08 +0530

    Initial commit

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const useCurrencyInfo = (currency) => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]))
+      .catch((error)=>console.log("error fetching data :", error ))
+  }, [currency]);
+ 
+  return data;
+};
+
+export {useCurrencyInfo};
